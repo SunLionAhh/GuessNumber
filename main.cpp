24eae10633cd0d644e92cca1e548a48be0064f91
Jerry <52823432+SunLionAhh@users.noreply.github.com>
2020-03-11 15:27:42 +0800

    美工完善！

--- main.cpp
+++ main.cpp
@@ -2,23 +2,26 @@
 #include <ctime>
 
 using namespace std;
-
+void guess(int num){
+    int value = 0;
+    //输入一个数字猜大小   过大 提示过大  过小 提示过小  相等  恭喜
+    while (true) {
+        cout<<"请输入你猜测的数字： ^_^"<<endl;
+        cin >> value;
+        if (value > num) {
+            cout << "猜测过大=_=" << endl;
+        } else if (value < num) {
+            cout << "猜测过小=_=" << endl;
+        } else {
+            cout << "恭喜你，猜对了^_^" << endl;
+            break;
+        }
+    }
+}
 int main() {
     //生成一个随机数
     srand((unsigned) time(0));
     int num = rand() % 100 + 1;
-    int value = 0;
-    //输入一个数字猜大小   过大 提示过大  过小 提示过小  相等  恭喜
-    while (true) {
-        cin >> value;
-        if (value > num) {
-            cout << "猜测过大" << endl;
-        } else if (value < num) {
-            cout << "猜测过小" << endl;
-        } else {
-            cout << "猜对了" << endl;
-            break;
-        }
-    }
+    guess(num);
     return 0;
 }
